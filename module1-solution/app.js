fd7ae485aaa5e9141231d302982542e432ade0be
Shajia Annoor <shajia.annoor@gmail.com>
2020-10-04 20:50:01 +0600

    Update empty string logic

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,24 +1,38 @@
 (function () {
     'use strict';
 
-    angular.module('joty-app',[])
-    .controller('LunchCheckController', LunchCheck);
+    angular.module('jotyapp',[])
+    .controller('LunchCheckController', LunchCheckController);
 
     LunchCheckController.$inject = ['$scope','$filter'];
 
-    function LunchCheck($scope, $filter) {
+    function LunchCheckController($scope, $filter) {
         $scope.message = "";
         $scope.foods = "" ; 
-    }
+    
 
-    $scope.CheckStatus = function() {
-        const food_names = $scope.foods.split(',')
-        if (food_names.length == 0 ) {
-            $scope.message = "Please enter data first" ; 
-        } else if ( food_names.length <= 3) {
-            $scope.message = "Enjoy!";
-        } else {
-            $scope.message = "Too much!";
-        }
-    };
+        $scope.CheckStatus = function() {
+            const food_names = $scope.foods.split(',')
+            let full = 0 ;
+            
+            for (let step = 0 ; step < food_names.length ; step ++ ) {
+
+                if(/\S/.test(food_names[step]) ) {
+                    full = 1 ; 
+                }
+            }
+
+            if ( full == 0 ) {
+                $scope.message = "Please enter data first" ;     
+                return
+            }
+            if (food_names.length == 1 && food_names[0] == "" ) {
+                $scope.message = "Please enter data first" ; 
+            } else if ( food_names.length <= 3) {
+                $scope.message = "Enjoy!";
+            } else {
+                $scope.message = "Too much!";
+            }
+        };
+    }
 })();
