cbbdef1e755fd95e801aa81a8ec6181c51c7177b
Shajia Annoor <shajia.annoor@gmail.com>
2020-10-04 20:10:46 +0600

    Add function to update message

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,16 +1,24 @@
 (function () {
     'use strict';
 
-    angular.module('jopu-app',[])
+    angular.module('joty-app',[])
     .controller('LunchCheckController', LunchCheck);
 
     LunchCheckController.$inject = ['$scope','$filter'];
 
     function LunchCheck($scope, $filter) {
-        $scope.message = ""
+        $scope.message = "";
+        $scope.foods = "" ; 
     }
 
     $scope.CheckStatus = function() {
-        
+        const food_names = $scope.foods.split(',')
+        if (food_names.length == 0 ) {
+            $scope.message = "Please enter data first" ; 
+        } else if ( food_names.length <= 3) {
+            $scope.message = "Enjoy!";
+        } else {
+            $scope.message = "Too much!";
+        }
     };
 })();
